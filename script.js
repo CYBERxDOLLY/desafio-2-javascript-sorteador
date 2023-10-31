2056cfd1357190542a6031274284843b358096b1
CYBERxDOLLY <brayansoaresdeaguiar@gmail.com>
2023-10-31 18:31:53 -0300

    Operadores de comparação adicionados

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 const drawButton = document.querySelector(".draw-button"); // armazena o botão de sorteio
 
 // função para realizar o sorteio de números aleatórios
-function drawNumber (){
+function drawNumber() {
     const min = Math.ceil(document.querySelector(".input-min").value);
     const max = Math.floor(document.querySelector(".input-max").value);
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
-
     const resultElement = document.getElementById("result-number");
-    
-    // classe result-hidden para ocultar o resultado
-    resultElement.classList.add("result-hidden");
 
-    setTimeout(() => {
-        resultElement.style.opacity = 1;
-        resultElement.innerHTML = result;
-        // remove a classe result-hidden para exibir o resultado com a transição
-        resultElement.classList.remove("result-hidden");
-    }, 100);    
+    if (min >= max) {
+        alert("Valor mínimo não pode ser maior ou igual ao valor máximo!")
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+        // classe result-hidden para ocultar o resultado
+        resultElement.classList.add("result-hidden");
+        setTimeout(() => {
+            resultElement.style.opacity = 1;
+            resultElement.innerHTML = result;
+            // remove a classe result-hidden para exibir o resultado com a transição
+            resultElement.classList.remove("result-hidden");
+        }, 100);
+    };
 };
 
 // função para tocar o arquivo de áudio ao clicar no botão
-function playAudio () {
+function playAudio() {
     const audio = document.getElementById("myAudio")
     audio.play()
 }
